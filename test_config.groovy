654bdff9c91a36dfa168de8b8ed3524dbb11f187
Marta Sienkiewicz <msienkiewicz7@gmail.com>
2025-05-13 20:56:18 +0200

    Modify test_config

--- test_config.groovy
+++ test_config.groovy
@@ -3,12 +3,14 @@ pipeline {
     stages {
         stage('Change config') {
             steps {
-                config = readFile "config.cfg"
-                echo ${config}
-                newconfig = config.replaceAll("PLACEHOLDER_VALUE", "new_value")
-                writeFile file: "config.cfg", text: newconfig
-                updated_config = readFile "config.cfg"
-                echo ${updated_config}
+                script {
+                    config = readFile "config.cfg"
+                    echo "${config}"
+                    newconfig = config.replaceAll("PLACEHOLDER_VALUE", "new_value")
+                    writeFile file: "config.cfg", text: newconfig
+                    updated_config = readFile "config.cfg"
+                    echo "${updated_config}"
+                }
             }
         }
     }
