631284ec20e77576ce7a99d1b1e44742759789f4
Marta Sienkiewicz <msienkiewicz7@gmail.com>
2026-08-14 12:20:53 +0200

    Change linter to declarative

--- jenkins/groovy-lint.groovy
+++ jenkins/groovy-lint.groovy
@@ -1,21 +1,17 @@
-"""
-Minimal Groovy linter, requires Docker Pipeline plugin.
-"""
-properties([
-    parameters([
+/* Minimal Groovy linter, requires Docker Pipeline plugin. */
+pipeline {
+    agent any
+    parameters {
         booleanParam(name: 'VERBOSE', defaultValue: false, description: 'Show verbose linting output')
-    ])
-])
-
-node {
-    checkout scm
-
-    stage('Groovy Lint') {
-        agent any
-
-        steps {
-            docker.image('nvuillam/npm-groovy-lint:v18.0.0').inside {
-                sh "npm-groovy-lint jenkins/**/*.groovy --failon error ${params.VERBOSE ? '--verbose' : ''}"
+    }
+    stages {
+        stage('Groovy Lint') {
+            steps {
+                script {
+                    docker.image('nvuillam/npm-groovy-lint:v18.0.0').inside {
+                        sh "npm-groovy-lint jenkins/**/*.groovy --failon error ${params.VERBOSE ? '--verbose' : ''}"
+                    }
+                }
             }
         }
     }
